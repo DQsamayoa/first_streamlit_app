b69d980867e1f42a342c5dd4f6dbe50e4ae6090e
Victor Samayoa <vasamayoa@gmail.com>
2023-11-16 21:45:04 -0600

    Update streamlit_app.py
    Fixed typo in the use of pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # Convert json to pandas data frame
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
